45a4b1a8229360265cf342b6715555b8fb0ffae1
Alex <a.nizhegorodtsev@gmail.com>
2018-12-06 21:26:01 +0100

    New screens

--- app/src/main/java/org/calendar/activity/DayActivity.java
+++ app/src/main/java/org/calendar/activity/DayActivity.java
@@ -1,26 +1,49 @@
 package org.calendar.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import org.calendar.R;
 
-public class DayActivity extends AppCompatActivity {
+public class DayActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String BUTTON_ID = "BUTTON_ID";
-    public static final String BACKGROUND_PREFIX = "background_";
+    public static final String BACKGROUND_PREFIX = "day_layout_";
+
+    private QuestManager questManager;
+    private String buttonId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_day);
+        questManager = QuestManager.getInstance(this);
+        buttonId = getIntent().getStringExtra(BUTTON_ID);
 
-        String buttonId = getIntent().getStringExtra(BUTTON_ID);
+        int layoutResourceId = getResources().getIdentifier(BACKGROUND_PREFIX + buttonId, "layout", this.getPackageName());
+        LinearLayout layout = (LinearLayout) findViewById(R.id.root_layout);
 
-        int drawableResourceId = getResources().getIdentifier(BACKGROUND_PREFIX + buttonId, "drawable", this.getPackageName());
+        LayoutInflater layoutInflater = (LayoutInflater)
+                this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = layoutInflater.inflate(layoutResourceId, layout, false);
+        layout.addView(v);
 
-        ImageView backgroundImage = findViewById(R.id.iv_background);
-        backgroundImage.setImageResource(drawableResourceId);
+        CheckBox checkBox = v.findViewById(R.id.cb_quest_complete);
+        checkBox.setChecked(questManager.getComplete(buttonId));
+        checkBox.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        CheckBox checkBox = (CheckBox) view;
+        Log.w("onClick", "onClick: " + buttonId + " state: " + checkBox.isChecked());
+        questManager.setComplete(buttonId, checkBox.isChecked());
     }
 }
